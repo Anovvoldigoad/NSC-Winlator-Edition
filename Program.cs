8896d78542ad94e6228d63be95112105ddb3632c
Anovvoldigoad <nopalfree1@gmail.com>
2026-06-19 09:23:04 +0700

    Remove HTTP server (incompatible with Winlator), use Flutter file I/O instead

--- Program.cs
+++ Program.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Threading.Tasks;
 using NSC.Winlator.Infrastructure;
 using NSC.Winlator.Services;
 
@@ -7,7 +6,7 @@ namespace NSC.Winlator
 {
     class Program
     {
-        static async Task Main(string[] args)
+        static void Main(string[] args)
         {
             Console.WriteLine("NSC Winlator Edition v1.0.0");
             Console.WriteLine("Initializing...");
@@ -19,12 +18,7 @@ namespace NSC.Winlator
                 Console.WriteLine("✓ Storage initialized");
                 Console.WriteLine($"✓ App folder: {AppBootstrap.ApplicationFolder}");
                 
-                // Start HTTP server in background
-                var httpServer = new HttpServerService(5000);
-                _ = Task.Run(() => httpServer.Start());
-                
                 Console.WriteLine("\nMod Manager Ready!");
-                Console.WriteLine("HTTP API: http://localhost:5000");
                 Console.WriteLine("Type 'help' for commands...\n");
                 
                 CommandHandler handler = new CommandHandler();
